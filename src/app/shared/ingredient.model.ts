818751f5978e2381076fd2fb66ef4eca6bf08afc
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-14 14:18:03 +0300

    add shopping list component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    //shortcut in the constructor
+    constructor(public name:string, public amount: number){}
+}
