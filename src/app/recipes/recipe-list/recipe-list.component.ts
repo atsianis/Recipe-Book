e93e437612c70edbed77811c7c67d43f77c3f877
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-14 15:23:14 +0300

    add click to recipe functionality
    A click on a recipe item emits an event that is tranfered 2 levels up, through recipe-list component, to recipes component and is handled there to produce the correct info in the recipe-details component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','test','https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13.jpg')
+    new Recipe('A Test Recipe','test','https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13.jpg'),
+    new Recipe('An other Test Recipe','test2','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRNYQQ4cKBHDkYH2OSw50lSVSrqsUBddUrTBw&usqp=CAU')
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelect(recipe:Recipe){
+    this.recipeSelected.emit(recipe);
   }
 
 }
