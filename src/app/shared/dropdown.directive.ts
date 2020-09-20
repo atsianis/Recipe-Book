05701709216cc8271fbf605f8e8b7c1305d20948
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-20 03:38:51 +0300

    enhance dropdown functionality

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,11 +6,17 @@ import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@an
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen(){
-    this.isOpen = !this.isOpen
-  }
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  // This closes the menu only on clicking on the button
+  // @HostListener('click') toggleOpen(){
+  //   this.isOpen = !this.isOpen
+  // }
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  
+  constructor(private elRef: ElementRef) { }
 
   ngOnInit(): void {
     
