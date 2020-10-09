aa3265c13ffe117f8745b2f66666e3585ce49c6c
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-09 17:00:10 +0300

    create reactive form for recipe editting and adding

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,17 +11,55 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id:number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params:Params) => {
         this.id = +params['id'];
-        this.editMode = params['id'] != null; 
+        this.editMode = params['id'] != null;
         // above we check if there is an id in the url, if there is, then we are in edit mode
+        this.initForm();
       }
-    )
+    );
+  }
+
+  private initForm() {
+    let recipeName =  '';
+    let recipeImagePath = '';
+    let description = '';
+    let ingredients = new FormArray([]);
+
+    if( this.editMode ) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      description = recipe.description;
+      if ( recipe['ingredients'] ) {
+        for( let ingredient of recipe.ingredients ){
+          ingredients.push(new FormGroup({
+            'name': new FormControl(ingredient.name),
+            'amount': new FormControl(ingredient.amount)
+          }))
+        }
+      }
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName,Validators.required),
+      'imagePath': new FormControl(recipeImagePath,Validators.required),
+      'description': new FormControl(description,Validators.required),
+      'ingredients': ingredients
+    })
+  }
+
+  get ingredientControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onSubmit(){
+    console.log(this.recipeForm);
   }
 
 }
