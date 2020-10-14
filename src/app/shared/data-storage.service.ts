896708a37929da1b930f89edbf6b15158de408c8
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-14 03:06:20 +0300

    add DataStorageService

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+
+  constructor(private http: HttpClient, private recipesService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http.put('https://recipebook-d75ae.firebaseio.com/recipes.json',recipes).subscribe( response => {
+      console.log(response);
+    });
+  }
+
+}
