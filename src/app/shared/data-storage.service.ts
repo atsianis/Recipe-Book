fe39414b8778fac3cfda1f1095771701bf86fd2e
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-14 13:29:29 +0300

    fix bug for fetching recipes that dont have ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -16,7 +17,18 @@ export class DataStorageService {
   }
 
   fetchRecipes(){
-    this.http.get<Recipe[]>('https://recipebook-d75ae.firebaseio.com/recipes.json').subscribe( recipes => {
+    this.http
+    .get<Recipe[]>(
+      'https://recipebook-d75ae.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map( recipes => {
+          return recipes.map(recipe => {
+            return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          })
+        })
+      )
+      .subscribe( recipes => {
       this.recipesService.setRecipes(recipes);
     })
   }
