6438ff3c8a0a6ce06f7139e3c2e8d4a977ce1aa7
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-16 01:16:57 +0300

    improve ui by showing the error message while signing up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -32,9 +32,9 @@ export class AuthComponent{
           console.log(responseData);
           this.isLoading = false;
       },
-        error => {
-          console.log(error);
-          this.error = 'An Error occured';
+        errorMessage => {
+          console.log(errorMessage);
+          this.error = errorMessage;
           this.isLoading = false;
         }
       );
