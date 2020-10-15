2b36032ff8af71814a0174c855ecc8a2523ca30b
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-16 02:59:38 +0300

    attach the user token to the fetchData reqiest

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipesService: RecipeService) {}
+  constructor(private http: HttpClient, private recipesService: RecipeService, private authService: AuthService) {}
 
   storeRecipes() {
     const recipes = this.recipesService.getRecipes();
@@ -24,21 +25,42 @@ export class DataStorageService {
   a recipe without adding any ingredients the ingredients array is not even initialized ), an empty one is created
   in order to avoid errors while manipulating the fetched recipe.
   */
+
+  /*
+  There are also a couple of strange thigs happening here, after alteration of the method,
+  that I'll try to explain here for future reading of the project
+  1) We are subscribing to a BehavioralSubject, which gives us the opportunity to retrieve the previous state of
+      it without having to subscribe to it like we do with regular Subjects. We do that by using the take RxJS operator.
+      In simple words, what it does is that we only want to take 1 value from the observable and then automatically
+      unsubscribe (it handles the subscription automatically). We are not getting informed for future changes here.
+      We are only interested on values ON DEMAND !!
+  2) We use the exhaustMap operator. What it does is that it WAITS until the previous observable is done and
+      then essentially it REPLACES IT COMPLETELY with the observable inside it. So in the end the observable
+      that is returned here is of http type, its the inside observable.
+  3) Lastly, the reason why we did all the previous was because we wanted to retrieve the logged in user, so
+      that we access his token and attach it to the http request that we are sending to Firebase. And that is exactly
+      what we are doing in the second parameter of the request, where we parametrize it.
+  */
   fetchRecipes(){
-    return this.http
-    .get<Recipe[]>(
-      'https://recipebook-d75ae.firebaseio.com/recipes.json'
-      )
-      .pipe(
-        map( recipes => {
-          return recipes.map(recipe => {
-            return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-          })
-        }),
-        tap ( recipes => {
-          this.recipesService.setRecipes(recipes);
-        })
-      )
+    return this.authService.userSubject.pipe(take(1), exhaustMap( user => {
+      return this.http.get<Recipe[]>(
+        'https://recipebook-d75ae.firebaseio.com/recipes.json',
+        {
+          params: new HttpParams().set('auth', user.token)
+        }
+      );
+    }),map( recipes => {
+      return recipes.map(recipe => {
+        return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+      })
+    }),tap ( recipes => {
+      this.recipesService.setRecipes(recipes);
+    })
+  )
+    }
+
+
+
   }
 
-}
+
