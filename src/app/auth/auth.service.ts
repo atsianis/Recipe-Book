e6e9616ff53d5db5a4bb0aea6b4aca708fef4854
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-16 17:40:37 +0300

    create autoLogin method in authService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -48,6 +48,20 @@ export class AuthService {
       }));
   }
 
+  autoLogin() {
+    const userData:
+    { email: string, id: string, _token: string, _tokenExpirationDate: string}
+      = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.userSubject.next(loadedUser);
+    }
+  }
+
   logout() {
     this.userSubject.next(null);
     this.router.navigate(['/auth']);
