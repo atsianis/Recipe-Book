3bf7a0347ae574481d670449dec5ec7d94ca8059
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-16 03:03:56 +0300

    improve explanatory comment

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -29,7 +29,7 @@ export class DataStorageService {
   /*
   There are also a couple of strange thigs happening here, after alteration of the method,
   that I'll try to explain here for future reading of the project
-  1) We are subscribing to a BehavioralSubject, which gives us the opportunity to retrieve the previous state of
+  1) We are subscribing to a BehaviorSubject, which gives us the opportunity to retrieve the previous state of
       it without having to subscribe to it like we do with regular Subjects. We do that by using the take RxJS operator.
       In simple words, what it does is that we only want to take 1 value from the observable and then automatically
       unsubscribe (it handles the subscription automatically). We are not getting informed for future changes here.
@@ -37,9 +37,11 @@ export class DataStorageService {
   2) We use the exhaustMap operator. What it does is that it WAITS until the previous observable is done and
       then essentially it REPLACES IT COMPLETELY with the observable inside it. So in the end the observable
       that is returned here is of http type, its the inside observable.
-  3) Lastly, the reason why we did all the previous was because we wanted to retrieve the logged in user, so
-      that we access his token and attach it to the http request that we are sending to Firebase. And that is exactly
-      what we are doing in the second parameter of the request, where we parametrize it.
+  3) Lastly, the reason why we did all the previous was because we wanted to retrieve the logged in user
+      (from the first observable), so that we access his token and attach it to the
+      http request that we are sending to Firebase. And that is exactly
+      what we are doing in the second parameter of the request (remember that the last argument of all http
+      requests is an object that is used to parametrize the request - add headers, parameters etc etc).
   */
   fetchRecipes(){
     return this.authService.userSubject.pipe(take(1), exhaustMap( user => {
