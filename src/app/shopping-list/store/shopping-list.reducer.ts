96b79edada727600cf2283c55c6c5edd380c3d2e
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-18 21:53:33 +0300

    create shoppingListReducer

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,4 +1,5 @@
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from '../../shared/ingredient.model';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [
@@ -12,6 +13,17 @@ const initialState = {
 // thats a TS feature, we can assign default values to function parameters in case they are not given by the caller
 // so the first time the app state will be set to the default value we give it but for subsequent calls
 // the state will always be the previous state
-export function shoppingListReducer(state = initialState, action) {
-
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
+  switch(action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      // copy of state
+      // remember : THE UPDATE OF STATE MUST HAPPEN IMMUTABLY
+      // WE MUST NOT TOUCH THE STATE OBJECT
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    default:
+      return state;
+  }
 }
