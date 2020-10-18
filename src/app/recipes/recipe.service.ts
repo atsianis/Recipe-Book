4773a25f5f88d270e9b2aece5861ecd641169913
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-18 23:38:51 +0300

    use NgRx approach to send recipes' ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,13 +3,19 @@ import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
+
 
 @Injectable()
 export class RecipeService{
 
     recipesChanged = new Subject<Recipe[]>();
 
-    constructor(private shoppingListService: ShoppingListService){}
+    constructor(
+      private shoppingListService: ShoppingListService,
+      private store: Store<{shoppingList: {ingredients: Ingredient[]}}>
+      ){}
 
     // private recipes: Recipe[] = [
     //     new Recipe('Chicken Wings',
@@ -53,8 +59,9 @@ export class RecipeService{
     }
 
     sendIngredientsToShoppingList(ingredients: Ingredient[]) {
-            this.shoppingListService.addIngredients(ingredients)
-        }
+      //this.shoppingListService.addIngredients(ingredients)
+      this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
+    }
 
 
 }
