590120ac9740c14fe8e789e3589ef4feba1d3ec5
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-21 23:05:23 +0300

    clean up the auth service to only handle the auto-logout timer

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,6 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
 import { User } from './user.model';
-import { environment } from '../../environments/environment';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
@@ -26,80 +22,20 @@ export class AuthService {
   private tokenExpirationTimer: any;
 
   constructor(
-    private http: HttpClient,
-    private router: Router,
     private store: Store<fromApp.AppState>
   ) {}
 
-  autoLogin() {
-    const userData:
-    { email: string, id: string, _token: string, _tokenExpirationDate: string}
-      = JSON.parse(localStorage.getItem('userData'));
-    if (!userData) {
-      return;
-    }
-    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
-
-    if (loadedUser.token) {
-      //this.userSubject.next(loadedUser);
-      this.store.dispatch( new AuthActions.AuthenticateSuccess({email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate)}))
-      const timeRemaining = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
-      this.autoLogout(timeRemaining);
-    }
-  }
-
-  autoLogout(expirationDuration: number) {
+  setLogoutTimer(expirationDuration: number) {
     this.tokenExpirationTimer = setTimeout( () => {
-      this.logout();
+      this.store.dispatch(new AuthActions.Logout());
     }, expirationDuration)
   }
 
-  logout() {
-    //this.userSubject.next(null);
-    this.store.dispatch( new AuthActions.Logout());
-    localStorage.removeItem('userData');
-    // making sure to clear the expiration timer (if existing) when manually logging out
+  clearLogoutTimer() {
     if (this.tokenExpirationTimer) {
       clearTimeout(this.tokenExpirationTimer);
+      this.tokenExpirationTimer = null;
     }
-    this.tokenExpirationTimer = null;
   }
 
-  private handleAuthentication( email: string, userId: string, token: string, expiresIn: number ){
-    const expirationDate = new Date( new Date().getTime() + expiresIn * 1000);
-    const user = new User(email, userId, token, expirationDate);
-    //this.userSubject.next(user);
-    this.store.dispatch( new AuthActions.AuthenticateSuccess({email: email, userId: userId, token: token, expirationDate: expirationDate}));
-    this.autoLogout(expiresIn * 1000);
-    localStorage.setItem('userData', JSON.stringify(user));
-  }
-
-  private handleError(errorResponse: HttpErrorResponse) {
-    let errorMessage = 'An unknown error occured';
-        if(!errorResponse.error || !errorResponse.error.error){
-          return throwError(errorMessage);
-        }
-        switch (errorResponse.error.error.message) {
-          // see firebase api for potential messages of sign in/up
-          case 'EMAIL_NOT_FOUND':
-            errorMessage = 'This email doesn\'t exists';
-            break;
-          case 'INVALID_PASSWORD':
-            errorMessage = 'The email was found but the password is incorrect';
-            break;
-          case 'USER_DISABLED':
-            errorMessage = 'The user account has been disabled by an administrator';
-            break;
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email already exists';
-              break;
-            case 'OPERATION_NOT_ALLOWED':
-              errorMessage = 'Operation not allowed';
-              break;
-            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-              errorMessage = 'Requests have been blocked due to unusual activity. Try again later';
-              break;
-        }
-        return throwError(errorMessage);
-  }
 }
