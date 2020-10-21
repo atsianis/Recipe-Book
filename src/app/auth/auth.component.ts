c915081a04bbb90f9467cc83be4197e8367852f3
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-21 03:32:29 +0300

    start the replace of login implementation from service-based to Ngrx-based

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,13 @@
-import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 import { AuthService, AuthResponseData } from './auth.service';
+import * as fromApp from '../store/app.reducer';
 import { Store } from '@ngrx/store';
+import * as AuthActions from './store/auth.actions';
 
 @Component({
   selector: 'app-auth',
@@ -48,23 +50,25 @@ export class AuthComponent implements OnInit,OnDestroy{
     let authObservable: Observable<AuthResponseData>;
 
     if (this.loginMode){
-      authObservable = this.authService.login(email,password)
+      //authObservable = this.authService.login(email,password)
+      this.store.dispatch(new AuthActions.LoginStart({email: email, password: password}));
     } else{
       authObservable = this.authService.signUp(email,password)
     }
-    authObservable.subscribe(
-      responseData => {
-        console.log(responseData);
-        this.isLoading = false;
-        this.router.navigate(['/recipes']);
-    },
-      errorMessage => {
-        console.log(errorMessage);
-        this.error = errorMessage;
-        this.showErrorAlert(errorMessage);
-        this.isLoading = false;
-      }
-    );
+
+    // authObservable.subscribe(
+    //   responseData => {
+    //     console.log(responseData);
+    //     this.isLoading = false;
+    //     this.router.navigate(['/recipes']);
+    // },
+    //   errorMessage => {
+    //     console.log(errorMessage);
+    //     this.error = errorMessage;
+    //     this.showErrorAlert(errorMessage);
+    //     this.isLoading = false;
+    //   }
+    // );
     form.reset();
   }
 
