063ed43e69ca92c1687f06868959344b4ee12366
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-21 22:46:52 +0300

    implement autoLogin logic in authReducer

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -6,6 +6,7 @@ import { environment } from '../../../environments/environment';
 import { of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../user.model';
 
 // 1.
 
@@ -51,6 +52,8 @@ const handleAuthentication = (
   token: string
 ) => {
   const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+  const user = new User(email, userId, token, expirationDate);
+  localStorage.setItem('userData', JSON.stringify(user));
   return new AuthActions.AuthenticateSuccess({
     email: email,
     userId: userId,
@@ -91,6 +94,7 @@ const handlError = (errorResponse) => {
 
 @Injectable()
 export class AuthEffects {
+
   @Effect()
   authSignUp = this.actions$.pipe(
     ofType(AuthActions.SIGNUP_START),
@@ -161,6 +165,39 @@ export class AuthEffects {
       this.router.navigate(['/']);
     })
   );
+
+  @Effect({dispatch: false})
+  authLogout = this.actions$.pipe(
+    ofType(AuthActions.LOGOUT),
+    tap( () => {
+      localStorage.removeItem('uderData');
+    })
+  );
+
+  @Effect()
+  autoLogin = this.actions$.pipe(
+    ofType(AuthActions.AUTO_LOGIN),
+    map( () => {
+      const userData:
+    { email: string, id: string, _token: string, _tokenExpirationDate: string}
+      = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return {type: 'no-auto-login'};
+    }
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      //this.userSubject.next(loadedUser);
+      return new AuthActions.AuthenticateSuccess({email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userData._tokenExpirationDate)});
+    //   const timeRemaining = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+    //   this.autoLogout(timeRemaining);
+    // }
+    }
+    return {type: 'no-auto-login'};
+  })
+
+  )
+
 
   constructor(
     private actions$: Actions,
