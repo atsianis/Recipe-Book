1a04db45fc8d5197469de9c4333b75725e5623bc
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-23 12:08:01 +0300

    load recipe-details through the Store

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,6 +3,9 @@ import { Recipe } from '../recipe.model';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,14 +16,30 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute,
-    private router: Router) { }
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<fromApp.AppState>) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        this.recipe = this.recipeService.getRecipe(this.id);
+        //this.recipe = this.recipeService.getRecipe(this.id);
+
+        // we have an observable inside an observable here
+        // we could use switch map to handle it as one observable
+        // remember: simply put, switch map exchanges the outside observable with the inside observable
+        this.store.select('recipes').pipe(map(
+          recipesState => {
+            return recipesState.recipes.find((recipe, index) => {
+              return index === this.id;
+            })
+          }
+        )).subscribe(recipe => {
+          this.recipe = recipe;
+        });
       }
     )
   }
