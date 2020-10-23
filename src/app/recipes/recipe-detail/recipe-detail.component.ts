303e4656cc22f2e45121f5405dadfe55b8f9c497
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-23 16:11:40 +0300

    use ngrx to implement add-Ingredients-to-shoppingList functionality

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { Ingredient } from 'src/app/shared/ingredient.model';
-import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../store/app.reducer';
 import { map } from 'rxjs/operators';
 import * as RecipesActions from '../store/recipe.actions';
+import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -18,7 +17,6 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(
-    private recipeService: RecipeService,
     private route: ActivatedRoute,
     private router: Router,
     private store: Store<fromApp.AppState>) { }
@@ -46,7 +44,8 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onAddToShoppingList(){
-    this.recipeService.sendIngredientsToShoppingList(this.recipe.ingredients);
+    //this.recipeService.sendIngredientsToShoppingList(this.recipe.ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(this.recipe.ingredients));
   }
 
   onDeleteRecipe() {
