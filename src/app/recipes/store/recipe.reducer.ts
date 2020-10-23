5c54ce7d86593f58dc98ecbff9915ed525628c76
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-10-23 14:41:29 +0300

    implement recipeReducer logic

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -16,6 +16,36 @@ export function recipeReducer( state = initialState, action: RecipesActions.Reci
         ...state,
         recipes: [...action.payload]
       }
+    case RecipesActions.ADD_RECIPE:
+      return {
+        ...state,
+        recipes:[...state.recipes, action.payload]
+      }
+    case RecipesActions.UPDATE_RECIPE:
+      // find the original recipe that we want to update
+      // copy it so that we handle the state immutably
+      // replace all the fields of that recipe with the fields of the new Recipe
+      const updatedRecipe = {
+        ...state.recipes[action.payload.index],
+        ...action.payload.newRecipe
+      };
+
+      // insert the just updated recipe back in the recipes-list
+      // and exactly at the index of the previous one
+      const updatedRecipes = [...state.recipes];
+      updatedRecipes[action.payload.index] = updatedRecipe;
+
+      return {
+        ...state,
+        recipes: updatedRecipes
+      }
+    case RecipesActions.DELETE_RECIPE:
+      return {
+        ...state,
+        recipes: state.recipes.filter( (recipe,index) => {
+          return index !== action.payload;
+        })
+      }
     default:
       return state;
   }
